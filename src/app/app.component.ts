6e43f670a47f1032d59c248fa3188f6ee8b3b5ac
Amit Rana <amit.rana@agilitypr.com>
2022-02-22 05:12:31 +0530

    Template form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailform';
+  email: string = '';
+
+  onSubmit() {
+    console.log(this.email);
+  }
+
+  // constructor() {
+  //   setInterval(() => {
+  //     this.email += 'a';
+  //   }, 2000);
+  // }
 }
